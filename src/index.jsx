1ca12e4b6c83e7d354bccf12383d7148fa8b8327
hiroto-okumura <hiroto_okumura@yahoo.co.jp>
2022-11-26 06:08:25 +0900

    add RootRoute(entry point of router) for index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import { createBrowserRouter, RouterProvider, route } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
