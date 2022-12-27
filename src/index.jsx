48e92e6485c1e4d751075d741d495b21bbbed723
hiroto-okumura <hiroto_okumura@yahoo.co.jp>
2022-12-27 22:57:48 +0900

    add .jsx DeleteRecord comp

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { createBrowserRouter, RouterProvider, route } from "react-router-dom";
+import Contact from "./routes/contact";
 import ErrorPage from "./error-page";
 import "./index.css";
-import Root from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        //actionにeditAction指定することでedit.jsxに定義したActionが発火する。紐づけ。
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
   },
 ]);
 
